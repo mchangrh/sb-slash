457e3f0beb4c84997aa0b0fbc7c82afc63cf0c6a
Michael C <michael@mchang.name>
2021-08-21 15:44:52 -0400

    finalize github and invite commands

--- src/commands/github.js
+++ src/commands/github.js
@@ -3,13 +3,15 @@ const { InteractionResponseType } = require("discord-interactions");
 module.exports = {
   name: "github",
   description: "Get a link for the sb-slash GitHub",
-  execute: async ({ interacion, response }) =>
+  execute: async ({ response }) =>
     response({
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
-        embeds: {
-          description: `Visit the [GitHub](https://github.com/mchangrh/sb-slash) to report any issues, make any feature requests or fork the source code`
-        }
+        embeds: [{
+          description: "Visit the [GitHub](https://github.com/mchangrh/sb-slash) to report any issues, make any feature requests or fork the source code",
+          color: 0xff0000
+        }],
+        flags: 64 // hide
       }
     })
 };
--- src/commands/invite.js
+++ src/commands/invite.js
@@ -3,13 +3,15 @@ const { InteractionResponseType } = require("discord-interactions");
 module.exports = {
   name: "invite",
   description: "Get a link to invite sb-slash to your server",
-  execute: async ({ interacion, response }) =>
+  execute: async ({ response }) =>
     response({
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
-        embeds: {
-          description: `Click [here](https://sb-slash.mchang.workers.dev/invite) to invite me to your server`
-        }
+        embeds: [{
+          description: "Click [here](https://sb-slash.mchang.workers.dev/invite) to invite sb-slash to your server",
+          color: 0xff0000
+        }],
+        flags: 64 // hide
       }
     })
 };
