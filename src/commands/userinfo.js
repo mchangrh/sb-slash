c0d9b1fe203dc8f301d6239983e5b068a717b69a
Michael C <michael@mchang.name>
2021-09-01 13:44:30 -0400

    more robust userID check

--- src/commands/userinfo.js
+++ src/commands/userinfo.js
@@ -1,10 +1,12 @@
 const { InteractionResponseType } = require("discord-interactions");
 const { ApplicationCommandOptionType } = require("slash-commands");
-const { isValidUserUUID } = require("../util/sbc-util.js");
 const { formatUser, getLastSegmentTime } = require("../util/formatResponse.js");
 const { userComponents } = require("../util/components.js");
 const { invalidPublicID } = require("../util/invalidResponse.js");
 const { getUserInfo } = require("../util/min-api.js");
+
+const userIDRegex = new RegExp(/(?:^|sb.ltn.fi\/userid\/)([a-f0-9]{64})/);
+const hasValidUserUUID = (str) => userIDRegex.test(str);
 
 module.exports = {
   type: 1,
@@ -29,16 +31,17 @@ module.exports = {
     const publicid = ((interaction.data.options.find((opt) => opt.name === "publicid") || {}).value || "").trim();
     const hide = (interaction.data.options.find((opt) => opt.name === "hide") || {}).value;
     // check for invalid publicID
-    if (!isValidUserUUID(publicid)) return response(invalidPublicID);
+    if (!hasValidUserUUID(publicid)) return response(invalidPublicID);
+    const userID = publicid.match(userIDRegex)[1];
     // fetch
-    const parsedUser = await getUserInfo(publicid);
+    const parsedUser = await getUserInfo(userID);
     // get last segment time
     const timeSubmitted = await getLastSegmentTime(parsedUser.lastSegmentID);
     return response({
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
         content: formatUser(parsedUser, timeSubmitted),
-        components: userComponents(publicid, false),
+        components: userComponents(userID, false),
         flags: (hide ? 64 : 0)
       }
     });
