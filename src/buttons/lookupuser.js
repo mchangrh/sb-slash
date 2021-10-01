c2f8b15f0bdc55e691ceb77af10861655808c090
Michael C <michael@mchang.name>
2021-10-01 00:41:35 -0400

    fix lookup user

--- src/buttons/lookupuser.js
+++ src/buttons/lookupuser.js
@@ -1,15 +1,15 @@
 const { InteractionResponseType, InteractionResponseFlags } = require("discord-interactions");
-const { isValidUserUUID } = require("../util/sbc-util.js");
 const { formatUser, getLastSegmentTime } = require("../util/formatResponse.js");
 const { userComponents } = require("../util/components.js");
 const { invalidPublicID } = require("../util/invalidResponse.js");
 const { getUserInfo } = require("../util/min-api.js");
+const { strictCheck } = require("../util/userUUID.js"); 
 
 module.exports = {
   name: "lookupuser",
   execute: async ({ interaction, response }) => {
     const publicid = interaction.message.embeds[0].description.match(/(?:\*\*User ID:\*\*) `([a-f0-9]{64})`/)[1];
-    if (!isValidUserUUID(publicid)) return response(invalidPublicID);
+    if (!strictCheck(publicid)) return response(invalidPublicID);
     // fetch
     const parsedUser = await getUserInfo(publicid);
     // get last segment time
