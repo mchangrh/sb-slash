a20aa6ec2a8a76c8781305394d2b1c9872208daf
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2022-01-30 14:16:32 +0100

    Move the single-page check into util/components.js from /searchsegments
    this also fixes the "Lookup Segments" message-command showing buttons when there's only 1 page

--- src/commands/searchsegments.js
+++ src/commands/searchsegments.js
@@ -87,7 +87,7 @@ module.exports = {
       responseTemplate.data.content = "```json\n"+stringified+"```";
     } else {
       responseTemplate.data.embeds = [formatSearchSegments(videoID, body, {...filterObj, page, videoID})];
-      if (body && JSON.parse(body).segmentCount > 10) responseTemplate.data.components = searchSegmentsComponents(body);
+      responseTemplate.data.components = searchSegmentsComponents(body);
     }
     return response(responseTemplate);
   }
