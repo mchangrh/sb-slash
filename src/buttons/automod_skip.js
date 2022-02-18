4f07e3b5e5101875291603fff3f42967846c0aaf
Michael C <michael@mchang.name>
2022-02-18 15:01:41 -0500

    don't break on no footer

--- src/buttons/automod_skip.js
+++ src/buttons/automod_skip.js
@@ -3,7 +3,7 @@ const { sendAutoMod } = require("../util/automod.js");
 module.exports = {
   name: "automod_skip",
   execute: async ({ interaction, response }) => {
-    const category = interaction.message.embeds[0].footer?.text;
+    const category = interaction.message.embeds[0]?.footer?.text;
     const message = await sendAutoMod({category});
     return response(message);
   }
