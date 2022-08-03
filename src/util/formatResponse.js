596a83fd4d93b0559394d2254d4320d3a316fd3a
Michael C <michael@mchang.name>
2022-08-03 19:32:12 -0400

    remove load from /status

--- src/util/formatResponse.js
+++ src/util/formatResponse.js
@@ -274,14 +274,6 @@ const formatStatus = async (res) => {
         value: `${data.processTime} ms`,
         inline: true
       }, {
-        name: "5 Minute Load",
-        value: `${data.loadavg[0].toFixed(2)}/2`,
-        inline: true
-      }, {
-        name: "15 Minute Load",
-        value: `${data.loadavg[1].toFixed(2)}/2`,
-        inline: true
-      }, {
         name: "1 min /status requests",
         value: data.statusRequests,
         inline: true
